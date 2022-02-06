1b4187dae28f057b36ed956bc67cf26e39695943
Anna Morozova <a.morozova.nrw@gmail.com>
2022-02-06 12:02:56 +0200

    styling search results added

--- src/components/ResultBox.js
+++ src/components/ResultBox.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
 export default function ResultBox({ resultList }) {
-    return <div>
-        <img src='https://source.unsplash.com/1600x900/?space-shuttle' alt='space' />
-        {resultList.name}
-        <p>{resultList.manufacturer}</p>
-        <p>Crew: {resultList.crew}</p>
-    </div>;
+    return <div className='bg-white flex'>
+        <img className="h-16 w-16 object-cover ml-2 mb-2 rounded" src='https://source.unsplash.com/1600x900/?space-shuttle' alt='space' />
+        <div className='ml-2'> {resultList.name} <br></br>
+      {resultList.manufacturer} | Crew: {resultList.crew}</div>
+    </div>
 }
